4ad552353408f688740757fd9055582e115bc81d
i_laijianming <i_laijianming@cvte.com>
2020-10-16 09:30:25 +0800

    [modify]更新组件扫描路径

--- src/main/java/com/jianming/apm/config/ApmAutoConfig.java
+++ src/main/java/com/jianming/apm/config/ApmAutoConfig.java
@@ -1,7 +1,10 @@
 package com.jianming.apm.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
 
 /**
  * apm 配置类
@@ -9,6 +12,9 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  */
 @EnableAspectJAutoProxy
 @Configuration
+@ComponentScan(value = "com.jianming.apm",
+        includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})},
+        useDefaultFilters = false)
 public class ApmAutoConfig {
 
 }
